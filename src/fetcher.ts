060f3b39adbb507fd5101546bb16a6b0ea14cd8d
Kingsley Victor <javaprodigy56@gmail.com>
2022-02-08 01:14:15 +0100

    add method to fetch triad data

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -60,46 +60,50 @@ export class Fetcher {
     tokenC: Token,
     providerUrl?: string
   ): Promise<Triad> {
-    invariant(
-      tokenA._chainId === tokenB._chainId && tokenB._chainId === tokenC._chainId,
-      'must_be_on_same_chain'
-    );
-    const address = Triad.getAddress(tokenA, tokenB, tokenC, tokenA.chainId());
-    const abiInterface = new Interface(I3SwapTriad.abi);
-    const getReservesEncoded = abiInterface.getSighash('getReserves');
-    let [reserve0, reserve1, reserve2] = await fetchRpc(
-      providerUrl || URLS[tokenA._chainId],
-      {
-        method: 'eth_call',
-        jsonrpc: '2.0',
-        id: 1,
-        params: [{ to: address, data: getReservesEncoded }]
+    try {
+      invariant(
+        tokenA._chainId === tokenB._chainId && tokenB._chainId === tokenC._chainId,
+        'must_be_on_same_chain'
+      );
+      const address = Triad.getAddress(tokenA, tokenB, tokenC, tokenA.chainId());
+      const abiInterface = new Interface(I3SwapTriad.abi);
+      const getReservesEncoded = abiInterface.getSighash('getReserves');
+      let [reserve0, reserve1, reserve2] = await fetchRpc(
+        providerUrl || URLS[tokenA._chainId],
+        {
+          method: 'eth_call',
+          jsonrpc: '2.0',
+          id: 1,
+          params: [{ to: address, data: getReservesEncoded }]
+        }
+      );
+
+      if (typeof reserve0 === 'string') {
+        reserve0 = JSBI.BigInt(reserve0.replace('0x', ''));
       }
-    );
 
-    if (typeof reserve0 === 'string') {
-      reserve0 = JSBI.BigInt(reserve0.replace('0x', ''));
-    }
+      if (typeof reserve1 === 'string') {
+        reserve1 = JSBI.BigInt(reserve1.replace('0x', ''));
+      }
 
-    if (typeof reserve1 === 'string') {
-      reserve1 = JSBI.BigInt(reserve1.replace('0x', ''));
+      if (typeof reserve2 === 'string') {
+        reserve2 = JSBI.BigInt(reserve2.replace('0x', ''));
+      }
+      let balances = tokenA.sortsBefore(tokenB)
+        ? [reserve0, reserve1, reserve2]
+        : [reserve1, reserve0, reserve2];
+      balances = tokenB.sortsBefore(tokenC)
+        ? [balances[0], balances[1], balances[2]]
+        : [balances[0], balances[2], balances[1]];
+      return Promise.resolve(
+        new Triad(
+          new TokenAmount(balances[0], tokenA),
+          new TokenAmount(balances[1], tokenB),
+          new TokenAmount(balances[2], tokenC)
+        )
+      );
+    } catch (error: any) {
+      invariant(false, error.message);
     }
-
-    if (typeof reserve2 === 'string') {
-      reserve2 = JSBI.BigInt(reserve2.replace('0x', ''));
-    }
-    let balances = tokenA.sortsBefore(tokenB)
-      ? [reserve0, reserve1, reserve2]
-      : [reserve1, reserve0, reserve2];
-    balances = tokenB.sortsBefore(tokenC)
-      ? [balances[0], balances[1], balances[2]]
-      : [balances[0], balances[2], balances[1]];
-    return Promise.resolve(
-      new Triad(
-        new TokenAmount(balances[0], tokenA),
-        new TokenAmount(balances[1], tokenB),
-        new TokenAmount(balances[2], tokenC)
-      )
-    );
   }
 }
